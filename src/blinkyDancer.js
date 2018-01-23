77eec547859da54553b9144acd0d0a027a900f48
indegow <indegow@aol.com>
2018-01-22 18:05:44 -0800

    Refactored into pseudoclassical - all tests passing

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -5,15 +5,12 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   
-
 };
-
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-makeBlinkyDancer.prototype.oldStep = makeBlinkyDancer.prototype.step;
-
+makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
 
 makeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
